6ac780cb8b49eb7b4c3b11e7c32355c4c2151ef5
joelbobai <joelisaiahbobai@gmail.com>
2026-03-09 11:53:22 +0100

    first commit issueTicket up

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   name: "PureCalc",
   slug: "purecalc",
-  version: "1.0.0",
+  version: "1.1.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "purecalc",
